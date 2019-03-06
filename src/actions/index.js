6c621930a5ff5416dee4a7b884516983fd67b3be
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-06 14:17:08 -0600

    Initial commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,20 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+import axios from "axios";
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAILURE = "FETCH_FAILURE";
+
+export const fetchCharacters = () => dispatch => {
+  dispatch({ type: START_FETCH });
+  axios
+    .get("https://swapi.co/api/people")
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err.response);
+    });
+};
